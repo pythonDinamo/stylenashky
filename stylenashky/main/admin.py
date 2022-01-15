773d6c9aef6df71183c0c21ae02fc953af3063bf
oleg <olegpustovalov220@gmail.com>
2022-01-15 18:02:04 +0300

    add models.py add admin.py

--- stylenashky/main/admin.py
+++ stylenashky/main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Product, Customer, Contact, URL
 
-# Register your models here.
+admin.site.register(Product)
+admin.site.register(Customer)
+admin.site.register(Contact)
+admin.site.register(URL)
+
+
+
