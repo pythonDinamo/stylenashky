ce7ccd6e087e2c5065d76e639a7267cebd876993
Eugene <fakemail@fbgrtraqqrjjsaabvcbcvb2.com>
2022-01-16 16:56:51 +0300

    Models changes.

--- stylenashky/main/models.py
+++ stylenashky/main/models.py
@@ -20,11 +20,11 @@ class Product(models.Model):
 
 
 class Customer(models.Model):
-    phone = models.CharField(verbose_name='Номер телефона', max_length=25)
+    user_tel = models.CharField(verbose_name='Номер телефона', max_length=25)
     complete = models.BooleanField(verbose_name='Обработано', default=False)
 
     def __str__(self):
-        return self.phone
+        return self.user_tel
 
     class Meta:
         verbose_name = 'Клиент'
