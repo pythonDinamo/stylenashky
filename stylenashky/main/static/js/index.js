22da4e4ebe686d9bba980d0c6623ac5bfe3bc848
Eugene <fakemail@fbgrtraqqrjjsaabvcbcvb2.com>
2022-01-17 14:32:11 +0300

    Fixed bug in JS code phone format.

--- stylenashky/main/static/js/index.js
+++ stylenashky/main/static/js/index.js
@@ -20,13 +20,17 @@ function formatPhoneNumber(value) {
    return `${phoneNumber.slice(0, 4)}(${phoneNumber.slice(4, 6)})${phoneNumber.slice(6, 9)}-${phoneNumber.slice(9, 11)}-${phoneNumber.slice(11, 13)}`;
 }
 
-function phoneNumberFormatter(elementId) {
-    const inputField = document.getElementById(elementId);
-    const formattedInputValue = formatPhoneNumber(inputField.value);
-    inputField.value = formattedInputValue;
+function phoneNumberFormatter() {
+    let target = event.target;
+    const formattedInputValue = formatPhoneNumber(target.value);
+    target.value = formattedInputValue;
 }
 
-const phoneInputId = 'id_user_tel';
-const phoneInputField = document.getElementById(phoneInputId);
-phoneInputField.addEventListener('keydown', function() {phoneNumberFormatter(phoneInputId)});
-phoneInputField.addEventListener('keyup', function() {phoneNumberFormatter(phoneInputId)});
+
+const phoneInputName = 'user_tel';
+var phoneInputFields = document.getElementsByName(phoneInputName);
+
+for(var i = 0; i < phoneInputFields.length; i++) {
+    phoneInputFields[i].addEventListener('keydown', function() {phoneNumberFormatter()});
+    phoneInputFields[i].addEventListener('keyup', function() {phoneNumberFormatter()});
+}
