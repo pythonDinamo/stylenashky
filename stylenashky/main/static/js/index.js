50fd213435027217087f4860fbd79ce0ab2d1b7a
analyze-this <barristers@tut.by>
2022-01-19 15:01:33 +0300

    all new brand

--- stylenashky/main/static/js/index.js
+++
@@ -1,9 +0,0 @@
-let headerburger = document.querySelector('.header__burger');
-    headerburger.addEventListener('click', showMenu);
-
-function showMenu(){
-    let menuNav = document.querySelector('.header__menu');
-
-    headerburger.classList.toggle('active');
-    menuNav.classList.toggle('active');
-}
